d84599028b4c8f334cac41a0f598659d89805c87
tanzorr <alexx1984@ukr.net>
2020-07-18 10:35:44 +0300

    last commit

--- www/cms2/registration.php
+++ www/cms2/registration.php
@@ -50,35 +50,57 @@
         }
 
     }
+
+
 ?>
-
-
-    <!-- Navigation -->
+<!-- Navigation -->
     
     <?php  include "includes/navigation.php"; ?>
-    
- 
-    <!-- Page Content -->
-    <div class="container">
+
+<?php
+if(isset($_GET['lang']) && !empty($_GET['lang'])){
+    $_SESSION['lang'] = $_GET['lang'];
+    if(isset($_SESSION['lang']) && $_SESSION['lang'] ==$_GET['lang']){
+        echo "<script>loction.reload() </script>";
+
+        if(isset($_SESSION['lang'])){
+            include "includes/languages/".$_SESSION['lang'].".php";
+        }else{
+            include "includes/languages/en.php";
+        }
+    }
+
+}
+?>
+<!-- Page Content -->
+<div class="container">
+    <form action="" id="language_form" class="navbar-form navbar-right form-control" method="get">
+        <div class="form-group" >
+            <select name="lang" onchange="changeLanguage()" >
+                <option value="en" <?php if(isset($_SESSION['lang']) && $_SESSION['lang']=='en'){echo "selected";} ?>>English</option>
+                <option value="es"<?php if(isset($_SESSION['lang']) && $_SESSION['lang']=='es'){echo "selected";} ?>>Spanish</option>
+            </select>
+        </div>
+    </form>
     
 <section id="login">
     <div class="container">
         <div class="row">
             <div class="col-xs-6 col-xs-offset-3">
                 <div class="form-wrap">
-                <h1>Register</h1>
+                <h1><?php echo _REGISTER;?></h1>
                     <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                         <p><?php echo $message; ?></p>
                         <div class="form-group">
-                            <label for="username" class="sr-only">username</label>
+                            <label for="username" class="sr-only"><?php echo _USERNAME;?></label>
                             <input type="text" name="username" id="username"
                                    autocomplete="on"
                                    value="<?php echo isset($username)? $username:''?>"
-                                   class="form-control" placeholder="Enter Desired Username">
+                                   class="form-control" placeholder="<?php echo _USERNAME;?>">
                                 <p><?php echo isset($error['username'])? $error['username']:''?></p>
                         </div>
                          <div class="form-group">
-                            <label for="email" class="sr-only">Email</label>
+                            <label for="email" class="sr-only"><?php echo _EMAIL;?></label>
                             <input type="email" name="email" id="email"
                                    autocomplete="on"
                                    value="<?php echo isset($email)? $email:''?>"
@@ -87,12 +109,12 @@
                              <p><?php echo isset($error['email'])? $error['email']:''?></p>
                         </div>
                          <div class="form-group">
-                            <label for="password" class="sr-only">Password</label>
-                            <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                            <label for="password" class="sr-only"><?php echo _PASSWORD;?></label>
+                            <input type="password" name="password" id="key" class="form-control" placeholder="<?php echo _PASSWORD;?>">
                              <p><?php echo isset($error['password'])? $error['password']:''?></p>
                         </div>
                 
-                        <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                        <input type="submit" name="submit" id="btn-login" class="btn btn-primary btn-lg btn-block" value="<?php echo _REGISTER;?>">
                     </form>
                  
                 </div>
@@ -103,7 +125,11 @@
 
 
         <hr>
-
+    <script>
+        function changeLanguage() {
+            document.getElementById('language_form').submit()
+        }
+    </script>
 
 
 <?php include "includes/footer.php";?>
