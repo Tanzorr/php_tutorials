126504246d9ad19e93549ee0cbdb65154ad9c263
tanzorr <alexx1984@ukr.net>
2020-09-18 14:39:20 +0300

    edit page start

---
+++ www/gallery_cms/admin/edit_photo.php
@@ -0,0 +1,61 @@
+<?php include("includes/header.php"); ?>
+<?php if(!$session->is_signed_in()){redirect('login.php');} ?>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.html">SB Admin</a>
+        </div>
+        <!-- Top Menu Items -->
+        <?php include("includes/top_naw.php"); ?>
+        <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+        <?php include("includes/side_nav.php"); ?>
+        <!-- /.navbar-collapse -->
+    </nav>
+
+    <div id="page-wrapper">
+
+        <div class="container-fluid">
+
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Photos
+                        <small>Subheading</small>
+                    </h1>
+                    <div class="col-md-8">
+                        <div class="form-group">
+                            <input type="text" name="title" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="caption">Caption</label>
+                            <input type="text" name="captcha" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="caption">Altrnate Text</label>
+                            <input type="text" name="alt_text" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="caption">Description</label>
+                            <textarea cols="30" rows="10" name="description" class="form-control"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
